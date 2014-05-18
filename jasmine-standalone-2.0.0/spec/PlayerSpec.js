812e32cbb0a89f0e6764f57ea3e2e1d1991d6837
Scott123454 <scott123454@hotmail.co.uk>
2014-05-18 11:27:33 +0100

    Thermostat with basic features, work in progress

--- jasmine-standalone-2.0.0/spec/PlayerSpec.js
+++ jasmine-standalone-2.0.0/spec/PlayerSpec.js
@@ -1,21 +1,69 @@
-describe("Thermostat", function() {
+describe("thermostat", function() {
   var thermostat;
 
   beforeEach(function() {
-    thermostat = new Thermstat();
+    thermostat = new Thermostat();
   });
-});
+
 
   it('starts at 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20)
-  })
+  });
 
   it('starts on power saving mode', function(){
     expect(thermostat.powerSavingMode).toEqual(true);
   })
 
   describe('maximum temperature', function(){
+
     it('is 25 if PSM is on', function(){
-      expect(thermostat.maximumTemperature()).toEqual(true);
+      expect(thermostat.maximumTemperature()).toEqual(25);
     })
+
+    it('is 32 if PSM is off', function(){
+      thermostat.powerSavingMode = false;
+      expect(thermostat.maximumTemperature()).toEqual(32);
+    })
+
   })
+
+  describe('minimum temperature', function(){
+    it('is 10', function(){
+      expect(thermostat.minimumTemperature()).toEqual(10);
+    })
+
+
+    it('reduces the temperature if > 10', function(){
+      thermostat.cooler();
+      expect(thermostat.temperature).toEqual(19);
+    })
+
+     
+    it('does not reduce the temperature if <=10', function(){
+      thermostat.temperature = 10;
+      thermostat.cooler();
+      expect(thermostat.temperature).toEqual(10);
+    })
+
+   })
+
+  // describe('warming temperature', function(){
+
+
+  describe('power saving mode off', function(){
+      beforeEach(function(){
+        thermostat.powerSavingMode = false;
+      })
+      
+      it('increases the temperature if < 32', function(){
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(21);
+      })
+
+
+      
+
+  })
+
+})
+
--- jasmine-standalone-2.0.0/src/Thermostat.js
+++ jasmine-standalone-2.0.0/src/Thermostat.js
@@ -1,7 +1,24 @@
 function Thermostat() {
-	this.temperature=20
+	this.temperature = 20;
 	this.powerSavingMode = true;
 }
 
+Thermostat.prototype.maximumTemperature = function() {
+	if(this.powerSavingMode) return 25;
+	return 32
+};
 
+Thermostat.prototype.minimumTemperature = function() {
+	return 10;
+};
 
+Thermostat.prototype.cooler = function() {
+	if(this.temperature <= this.minimumTemperature()) return false
+	this.temperature -=1;	// body...
+};
+
+Thermostat.prototype.warmer = function() {
+	if(this.temperature >= this.maximumTemperature()) return false 
+	this.temperature +=1;
+	// body...
+};
